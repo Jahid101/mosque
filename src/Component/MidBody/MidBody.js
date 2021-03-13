3e5e77a16636aecdd1c848dfd1bcfec85e9b51ed
Jahid <jahidhasananik.official@gmail.com>
2021-03-13 11:39:14 +0600

    half done

--- src/Component/MidBody/MidBody.js
+++ src/Component/MidBody/MidBody.js
@@ -3,10 +3,43 @@ import bg5 from '../../image/bg5.jpg';
 import './MidBody.css';
 
 const MidBody = () => {
-    
+
+    const style = {
+        marginTop: '25px',
+        width: '90%',
+        height: '300px',
+        marginLeft: '5%',
+        borderRadius: '15px',
+        marginBottom: '25px',
+        backgroundImage: `URL(${bg5})`
+    }
+
     return (
-        <div className="timeBg">
-            <img src={bg5} alt=""/>
+        <div>
+            <div style={style} className="ml-5 mt-5">
+                <div>
+                    <input className="ml-4 mt-2" type="text" name="" id="" placeholder="Fazar" />
+                    <input className="ml-1" type="text" name="" id="" placeholder="Juhar" />
+                    <input className="ml-1" type="text" name="" id="" placeholder="Asar" />
+                    <input className="ml-1" type="text" name="" id="" placeholder="Magriv" />
+                    <input className="ml-1" type="text" name="" id="" placeholder="Esha" />
+                    <input className="ml-1" type="text" name="" id="" placeholder="Juma" />
+                    <button className="ml-4 mt-2 btn btn-success">Change</button>
+                </div>
+
+                <div className="d-flex time ml-5 mt-4">
+                    <div className="timeMargin">
+                        <h3>Fazar: 5.00 am</h3>
+                        <h3>juhar: 1.15 pm</h3>
+                        <h3>Asar: 4.15 pm</h3>
+                    </div>
+                    <div className="timeMargin">
+                        <h3>Magriv: 6.05 pm</h3>
+                        <h3>Esha: 8.15 pm</h3>
+                        <h3>Juma: 1.30 pm</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
